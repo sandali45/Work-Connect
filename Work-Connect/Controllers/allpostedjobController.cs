6ea82315408d3141c97754d23f71fcad04a558a4
sandali45 <139307298+sandali45@users.noreply.github.com>
2024-10-15 00:47:13 +0530

    Add the posted job page

---
+++ Work-Connect/Controllers/allpostedjobController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Work_Connect.Controllers
+{
+    public class allpostedjobController : Controller
+    {
+        public IActionResult allpostedjob()
+        {
+            return View();
+        }
+    }
+}
