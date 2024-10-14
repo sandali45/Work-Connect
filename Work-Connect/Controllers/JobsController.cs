56f7746b76dc8c4160304898185e58a2849ba2d6
Oneli <onithesanya@gmail.com>
2024-10-14 20:55:02 +0530

    Modified homecontroller,addedjobs controller, modified index.cs, modified search.cshtmll, added shared to views,modified contact,landing,search and site.css

---
+++ Work-Connect/Controllers/JobsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Work_Connect.Services;
+
+namespace Work_Connect.Controllers
+{
+    public class JobsController : Controller
+    {
+        private readonly JobService _jobService;
+
+        public JobsController(JobService jobService)
+        {
+            _jobService = jobService;
+        }
+
+        public async Task<IActionResult> Search(string searchQuery)
+        {
+            var jobs = await _jobService.SearchJobsBySkills(searchQuery);
+            ViewData["searchQuery"] = searchQuery; // Set the search query for the view
+            return View("Search", jobs); // Return the search results to the Search view
+        }
+    }
+}
--- Work-Connect/Models/Job.cs
+++ Work-Connect/Models/Job.cs
@@ -3,10 +3,10 @@
     public class Job
     {
         public int Id { get; set; } // Add an Id property for primary key
-        public string CompanyName { get; set; }
-        public string JobTitle { get; set; }
-        public string JobDescription { get; set; }
-        public string Skills { get; set; }
-        public string PaymentDetails { get; set; }
+        public string? CompanyName { get; set; }
+        public string? JobTitle { get; set; }
+        public string? JobDescription { get; set; }
+        public string? Skills { get; set; }
+        public string? PaymentDetails { get; set; }
     }
 }
