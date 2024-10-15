7c2d7c6339fafb67d288969cc015689253622a51
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2024-10-15 23:02:46 +0530

    Job Listing Front End, Css Controller,

---
+++ Work-Connect/Controllers/JobListingController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Work_Connect.Models; 
+using Work_Connect.Data; 
+
+public class JobListingController : Controller
+{
+    private readonly ApplicationDbContext _context; 
+
+    public JobListingController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        // Retrieve job listings from the database
+        List<Job> jobs = _context.Jobs.ToList(); //DbSet<Job> in the DbContext
+        return View(jobs); // Pass the list of jobs to the view
+    }
+}
