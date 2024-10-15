9c72f027723368de9a0889ee4a362f95c8e86889
sandali45 <139307298+sandali45@users.noreply.github.com>
2024-10-15 15:47:47 +0530

    Add database connection

--- Work-Connect/Controllers/PostJobController.cs
+++ Work-Connect/Controllers/PostJobController.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Work_Connect.Data;
+using Work_Connect.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Work_Connect.Controllers
 {
     public class PostJobController : Controller
     {
-        public IActionResult postJob()
+      
+        private readonly ApplicationDbContext _context; // Make sure to create this context
+
+        public PostJobController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
 
+        public async Task<IActionResult> postJob()
+        {
+            List<postAJob> jobPosts = await _context.postAJob.ToListAsync();
+            return View(jobPosts);
+        }
+        [HttpPost]
+       
         public IActionResult waitingForApproval()
         {
             return View();
--- Work-Connect/Controllers/allpostedjobController.cs
+++ Work-Connect/Controllers/allpostedjobController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Work_Connect.Data;
+using Work_Connect.Models;
+using Microsoft.EntityFrameworkCore; 
 
 namespace Work_Connect.Controllers
 {
     public class allpostedjobController : Controller
     {
-        public IActionResult allpostedjob()
+     
+        private readonly ApplicationDbContext _context; // Make sure to create this context
+
+        public allpostedjobController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> allpostedjob()
+        {
+            List<postAJob> jobPosts = await _context.postAJob.ToListAsync();
+            return View(jobPosts);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var jobPost = await _context.postAJob.FindAsync(id); // Find the job post by ID
+            if (jobPost == null)
+            {
+                return NotFound(); // Return 404 if not found
+            }
+
+            _context.postAJob.Remove(jobPost); // Remove the job post
+            await _context.SaveChangesAsync(); // Save changes to the database
+
+            return Json(new { success = true }); // Return success as JSON
         }
     }
 }
