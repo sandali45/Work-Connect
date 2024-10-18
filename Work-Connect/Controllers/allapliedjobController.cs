e937134c420864fa47468f05f7ffbef57166fcf8
sandali45 <139307298+sandali45@users.noreply.github.com>
2024-10-19 00:30:21 +0530

    Applied job changes

--- Work-Connect/Controllers/allapliedjobController.cs
+++ Work-Connect/Controllers/allapliedjobController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Work_Connect.Data;
+using Work_Connect.Models;
 
 namespace Work_Connect.Controllers
 {
     public class allapliedjobController : Controller
     {
-        public IActionResult allapliedjob()
+        private readonly ApplicationDbContext _context; // Make sure to create this context
+
+        public allapliedjobController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
+
+        public async Task<IActionResult> allapliedjob()
+        {
+            List<JobApplication> JobApplication = await _context.JobApplications.ToListAsync();
+            return View(JobApplication);
+        }
+
     }
 }
