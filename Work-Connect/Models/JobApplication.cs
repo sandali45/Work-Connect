ca44bd6380d92fd67ea7dd9584dbc4734c563524
sandali45 <139307298+sandali45@users.noreply.github.com>
2024-10-18 15:00:31 +0530

    Job list

---
+++ Work-Connect/Models/JobApplication.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Work_Connect.Models
+{
+    public class JobApplication
+    {
+        [Key] // This annotation specifies that ApplicationId is the primary key
+        public int ApplicationId { get; set; } // Primary key
+
+        public int JobID { get; set; } // Foreign key for the associated job
+
+        public string ApplicantName { get; set; } // Applicant's name
+
+        public string Resume { get; set; } // Base64 or file path for the resume
+    }
+}
