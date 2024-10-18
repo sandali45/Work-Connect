5729af648725f8d6cf054ced902cdbb0c264e700
sandali45 <139307298+sandali45@users.noreply.github.com>
2024-10-19 00:36:15 +0530

    Delete applied Jobs option

--- Work-Connect/Controllers/allapliedjobController.cs
+++ Work-Connect/Controllers/allapliedjobController.cs
@@ -19,6 +19,23 @@ namespace Work_Connect.Controllers
             List<JobApplication> JobApplication = await _context.JobApplications.ToListAsync();
             return View(JobApplication);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Remove(int id)
+        {
+            // Find the company by ID
+            var JobApplication = await _context.JobApplications.FindAsync(id);
+            if (JobApplication == null)
+            {
+                return NotFound(); // Return 404 if not found
+            }
+
+            // Remove the company from the context
+            _context.JobApplications.Remove(JobApplication);
+            await _context.SaveChangesAsync(); // Save changes to the database
+
+            return RedirectToAction(nameof(allapliedjob)); // Redirect back to the list
+        }
 
     }
 }
