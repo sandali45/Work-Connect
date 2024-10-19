58ca2dbab9bcb6074e3139f72f8fbd37159d5335
sandali45 <139307298+sandali45@users.noreply.github.com>
2024-10-19 17:17:55 +0530

    Admin users

---
+++ Work-Connect/Controllers/AdminUsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Work_Connect.Controllers
+{
+    public class AdminUsersController : Controller
+    {
+        public IActionResult AdminUsers()
+        {
+            return View();
+        }
+    }
+}
